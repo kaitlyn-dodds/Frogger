43edb83db75301bc033c2ddc7d9a487ba9becea6
kaitlyn-dodds <sub@kadodds.com>
2025-12-15 21:51:54 -0700

    set up some scene nodes w/ collision and organized game nodes

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,25 +2,26 @@ extends CharacterBody2D
 
 @onready var player_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 3000.0
+const SPEED = 4000.0
 const JUMP_VELOCITY = -400.0
 
+
+func setPlayerAnimation(direction: Vector2) -> void:
+	if direction == Vector2.ZERO:
+		player_sprite.play("idle")
+		return
+		
+	if direction.x != 0:
+		player_sprite.play("move_right" if direction.x > 0 else "move_left")
+	else:
+		player_sprite.play("move_down" if direction.y > 0 else "move_up")
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction 
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	
+
 	# set character animation 
-	if direction == Vector2.ZERO:
-		player_sprite.play("idle")
-	if direction == Vector2.LEFT:
-		player_sprite.play("move_left")
-	if direction == Vector2.RIGHT:
-		player_sprite.play("move_right")
-	if direction == Vector2.DOWN:
-		player_sprite.play("move_down")
-	if direction == Vector2.UP:
-		player_sprite.play("move_up")
+	setPlayerAnimation(direction)
 	
 	# set velocity
 	velocity = direction * SPEED * delta
